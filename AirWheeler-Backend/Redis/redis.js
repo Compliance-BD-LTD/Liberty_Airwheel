c82023b0b35c214ec34b2521d61e9c5aa2342536
sohanurrahmanabir07 <mdsohanurrahmanabir@gmail.com>
2026-01-19 12:23:11 +0600

    Added the redis database

--- AirWheeler-Backend/Redis/redis.js
+++ AirWheeler-Backend/Redis/redis.js
@@ -1,5 +1,5 @@
 const redis=require('redis')
-const client=redis.createClient()
+const client=redis.createClient({ url: 'redis://127.0.0.1:6379/1' })
 client.connect() 
         .then(() => console.log('Redis connected!'))
         .catch(err => console.error('Redis connection error:', err));
